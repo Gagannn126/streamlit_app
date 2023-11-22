d90c64320f022773dcffaa565d2d918a95c5df28
Gagannn126 <Gagannn126@gmail.com>
2023-11-22 16:55:42 +0530

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,19 +20,23 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice=streamlit.text_input('Type fruit to know more about it')
     if not fruit_choice:
         streamlit.error("Please select a fruit")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-        fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function=get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
 
-streamlit.stop()
+# streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -43,5 +47,7 @@ streamlit.header("Fruit list:")
 streamlit.dataframe(my_data_rows)
 
 streamlit.header("Add a fruit of your choice")
+
+
 add_fruit=streamlit.text_input('Type fruit to add','jackfruit')
 streamlit.text(add_fruit+' added')
